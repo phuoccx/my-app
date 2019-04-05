76eda0de767af9f0102c6b4d54c2a1850318080c
Phuoc Cao <phuoccx93@gmail.com>
2019-04-05 10:44:23 +0700

    done lession 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var i = 1;
+    var newStyle = {
+      fontSize : 35,
+      color : '#FF0000'
+    }
     return (
       <div className="App">
-      <h1>hello, im phuoc</h1>
+        <h1 style={newStyle}>Header</h1>
+        <h2>Content</h2>
+        <p>This is content data</p>
+        <p>{1+1}</p>
+        <p>{i === 1 ? 'var is 1' : 'false'}</p>
       </div>
     );
   }
 }
-
 export default App;
